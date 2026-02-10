cd3556bc68bcb84ee7525ceb083a313f58cab7b1
P_Kariuki007 <spicezke@gmail.com>
2026-02-10 17:41:33 +0300

    Update Activate.ps1

--- weather_project/venv/Scripts/Activate.ps1
+++ weather_project/venv/Scripts/Activate.ps1
@@ -203,8 +203,8 @@ else {
     }
     else {
         Write-Verbose "  Setting prompt based on parent's directory's name. (Is the directory name passed to venv module when creating the virtual environment)"
-        Write-Verbose "  Got leaf-name of $VenvDir='$(Split-Path -Path $venvDir -Leaf)'"
-        $Prompt = Split-Path -Path $venvDir -Leaf
+        Write-Verbose "  Got leaf-name of $VenvDir='$(Split-Path -Path $VenvDir -Leaf)'"
+        $Prompt = Split-Path -Path $VenvDir -Leaf
     }
 }
 
